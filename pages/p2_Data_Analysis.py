7f60d6a0ddc0fd23e91a6fc7d5de71a960ad9fe4
rudy desplan <desrudy@hotmail.com>
2025-03-22 10:30:43 +0100

    Create p2_Data_Analysis.py

---
+++ pages/p2_Data_Analysis.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv('fmi_weather_and_price_completed.csv')
+    df['Time'] = pd.to_datetime(df['Time'], format='%Y/%m/%d %H:%M:%S')
+    df = df.sort_values('Time')
+    return df
+
+def main():
+    st.title("Exploratory Data Analysis")
+    
+    try:
+        fmi_data = load_data()
+        
+        # --------------------------
+        # 1. Numerical Descriptive Stats
+        # --------------------------
+        st.header("Numerical Descriptive Statistics")
+        numerical_columns = fmi_data.drop(columns=['Time']).columns
+      
+        numeric_descriptive_stats = fmi_data.describe().T
+        st.dataframe(numeric_descriptive_stats.style.format("{:.2f}"), 
+                    height=600,
+                    use_container_width=True)
+
+if __name__ == "__main__":
+    main()
