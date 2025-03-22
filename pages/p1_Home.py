c4451a16e1e51d759473615b9cad65e609161604
rudy desplan <desrudy@hotmail.com>
2025-03-22 10:17:54 +0100

    Create p1_Home.py

---
+++ pages/p1_Home.py
@@ -0,0 +1,13 @@
+# pages/1_Home.py
+import streamlit as st
+
+def main():
+    st.title("Project Overview")
+    st.markdown("""
+    ## Weather conditions effect on Electriciy Price Analysis
+    **Dataset:** Nordpool Spot electric prices in Finland X Historical Weather data  
+    **Objective:** Study what kind of relationship weather conditions have with electricity prices in Finland.
+    """)
+    
+if __name__ == "__main__":
+    main()
