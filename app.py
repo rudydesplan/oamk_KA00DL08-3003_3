50dd0235cc5a0b54eef4cf5a101aea8c063abc6f
rudy desplan <desrudy@hotmail.com>
2025-03-22 09:35:16 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+import streamlit as st
+
+st.set_page_config(
+    page_title="Weather conditions affect analysis on Nordpool Spot electric prices in Finland",
+    page_icon="⚡🏠⚡",
+    layout="wide"
+)
+
+st.title("Weather conditions X Electricity prices Analysis")
+st.markdown("Navigate through pages using the sidebar 👈")
