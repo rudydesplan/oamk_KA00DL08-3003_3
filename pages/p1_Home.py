66cc0436973ba89fb9f9781966720eccf02b21a9
rudy desplan <desrudy@hotmail.com>
2025-03-22 10:18:53 +0100

    Update p1_Home.py

--- pages/p1_Home.py
+++ pages/p1_Home.py
@@ -5,7 +5,9 @@ def main():
     st.title("Project Overview")
     st.markdown("""
     ## Weather conditions effect on Electriciy Price Analysis
-    **Dataset:** Nordpool Spot electric prices in Finland X Historical Weather data  
+    
+    **Dataset:** Nordpool Spot electric prices in Finland X Historical Weather data
+    
     **Objective:** Study what kind of relationship weather conditions have with electricity prices in Finland.
     """)
     
